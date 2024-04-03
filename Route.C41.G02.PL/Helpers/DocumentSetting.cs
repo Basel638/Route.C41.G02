ee17afec1fdbae95e7ed165d126a27c4cc3aee84
Basel638 <basselsameh2004@gmail.com>
2024-04-03 23:11:05 +0200

    Part 01 Document Setting

---
+++ Route.C41.G02.PL/Helpers/DocumentSetting.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Route.C41.G02.PL.Helpers
+{
+	public class DocumentSetting
+	{
+        public static string  UploadFile(IFormFile file,string folderName)
+		{
+			// 1. Get Located Folder Path
+			//string folderPath = $"D:\\Route Course\\MVC\\Ahmed Nasr\\Session 03\\Demos\\Route.C41.G02.PL\\Route.C41.G02.PL\\wwwroot\\Files\\{folderName}";
+
+			//string folderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\Files\\{folderName}";
+
+			string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", folderName);
+
+			if(!Directory.Exists(folderPath))
+				Directory.CreateDirectory(folderPath);
+			
+			// 2. Get File Name and Make it UNIQUE
+
+			string fileName=$"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+			// 3. Get File Path
+			string filePath= Path.Combine(folderPath, fileName);
+
+			// 4. save file as streams[Data Per Time]
+			var fileStream = new FileStream(filePath, FileMode.Create);
+			file.CopyTo(fileStream);
+
+			return fileName;
+
+
+
+		}
+
+		public static void DeleteFile(string fileName,string folderName)
+		{
+			string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot//files", folderName);
+			string filePath = Path.Combine(folderPath, fileName);
+			if(File.Exists(filePath))
+				File.Delete(filePath);
+		}
+
+    }
+}
